2a14f3791c4b4486133a7fa8d4032805343b83de
pierrotws <m.sauvage.pierre@gmail.com>
2013-12-16 23:47:38 +0100

    big commit, start of serious works

--- java/src/main/java/fr/ece/neolog/Spooler.java
+++ java/src/main/java/fr/ece/neolog/Spooler.java
@@ -1,54 +1,38 @@
 package fr.ece.neolog;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.StringTokenizer;
-
+import javax.json.Json;
+import javax.json.JsonObject;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.MasterNotRunningException;
-import org.apache.hadoop.hbase.ZooKeeperConnectionException;
-import org.apache.hadoop.hbase.client.Get;
-import org.apache.hadoop.hbase.client.HBaseAdmin;
-import org.apache.hadoop.hbase.client.HTable;
-import org.apache.hadoop.hbase.client.Put;
-import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-import org.jsoup.Jsoup;
 
 
 public class Spooler {
 	/**
 	 * @param args : arg[0] = input file
 	 */
-	private static HTable dico = null;
-	private static HTable tracks = null;
+	
+	private static Dictionnary dico = null;
 	public static void main(String[] args) {
 		Path input = new Path((args.length>1) ? args[1] : "input");
 		Configuration conf = new Configuration();
 		Configuration hConf = HBaseConfiguration.create();
-		
 		try {
-			HBaseAdmin hbase = new HBaseAdmin(conf);
-		} catch (MasterNotRunningException | ZooKeeperConnectionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			System.out.println("Zookeeper et/ou HBase ne sont pas exécutés sur la machine");
-		}
-		try {
-			dico = new HTable(hConf,(args.length>0) ? args[0] : "dictionnary");
-			tracks = new HTable(hConf,"tracks");
+			dico = new Dictionnary(hConf,(args.length>0) ? args[0] : "dictionnary");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			System.out.println("la table '"+((args.length>0) ? args[0] : "dictionnary")+"' n'existe pas");
+			System.out.println("la table '"+((args.length>0) ? args[0] : "dictionnary")+"' n'existe pas, vérifier la conf Zookeeper et Hbase, et vérifier si HBase est exécuté");
 		}
 		System.out.println("*** Creating Job");
 		Job job;
@@ -84,34 +68,25 @@ public class Spooler {
 	}
 	
 	public static class Map extends Mapper<LongWritable, Text, Text, NullWritable> {
-	
 		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-			StringTokenizer text = new StringTokenizer(Jsoup.parse(value.toString()).select("body").text());
+			JsonObject json = Json.createReader(new StringReader(value.toString())).readObject();
+			StringTokenizer text = new StringTokenizer(json.getString("text"));
 			while(text.hasMoreTokens()){
-				checkHBaseTokenList(text.nextToken());
-			}
-		}
-		private void checkHBaseTokenList(String str){
-			byte[] rowKey = Bytes.toBytes(str);
-			Get row = new Get(rowKey);
-			try {
-				Result rs = dico.get(row);
-				//Si le resultat existe
-				if(!rs.isEmpty()){
-					System.out.println("OUAOU");
+				String word = text.nextToken().toLowerCase();
+				if(dico.isInDictionnary(word)){	
+					System.out.println(word+" is known");
+					if(dico.isTracked(word)){
+						System.out.println(word+" is tracked");
+						//dico.addTrack(word, json.getString("type"), json.getString("age"), json.getString("location"), json.getString("timestamp"));
+					}
 				}
 				else{
-					System.out.println("creating in table:"+str);
-					Put p = new Put(rowKey);
-					p.add(Bytes.toBytes("infos"), Bytes.toBytes("type"), Bytes.toBytes("n"));
-					p.add(Bytes.toBytes("infos"), Bytes.toBytes("track"), Bytes.toBytes("y"));
-					p.add(Bytes.toBytes("infos"), Bytes.toBytes("counter"), Bytes.toBytes(1));
-					dico.put(p);
+					System.out.println(word+" is not known");
+					//dico.addNeologism(word);
+					//dico.addTrack(word, json.getString("type"), json.getString("age"), json.getString("location"), json.getString("timestamp"));
 				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
 		}
 	}
+	
 }
