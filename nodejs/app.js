d02769c6f5b6cb8b7634be8e89f62f7fd5c83038
cesarBere <c.berezowski@gmail.com>
2013-12-16 14:30:27 +0100

    Added twit module + tests

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
+var twitter = require('./routes/twitter');
 var http = require('http');
 var path = require('path');
 
@@ -26,8 +27,13 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+//app.get('/', routes.index);
+//app.get('/users', user.list);
+app.get('/tweets/:word/:date/:count', function(req){
+	twitter.searchTweets(req.params.word, req.params.date, req.params.count, function(tweets){
+		console.log(tweets);
+	});
+})
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
