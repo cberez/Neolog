e73d99717e43fbd58a10da4e9a1732e280561008
cesarBere <c.berezowski@gmail.com>
2013-12-16 14:34:05 +0100

    .

--- nodejs/routes/twitter.js
+++ nodejs/routes/twitter.js
@@ -1,10 +1,10 @@
 var Twit = require('twit')
 
 var T = new Twit({
-    consumer_key:         'FuYs3eS7aAJarJSPnuX1w', 
-    consumer_secret:      'moyKVCG0wlPIkBFxTfA2ib1FQH8unJt1Qg7RCyJBkJ8', 
-    access_token:         '187591030-klpnU4Rx5JTsaRZYuUqs6mD4630eBtwrCkmovb1W', 
-    access_token_secret:  'q0NHXXPSYTZ7klCsOnWfAp1jhROK8Lyaxks5W24SZG927'
+    consumer_key:         '...', 
+    consumer_secret:      '...', 
+    access_token:         '...', 
+    access_token_secret:  '...'
 })
 
 module.exports = {
