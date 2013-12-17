1ab96b56da3f7dca40c51ec64d7b42c81308aa3a
cesarBere <c.berezowski@gmail.com>
2013-12-17 10:56:17 +0100

    Added hbase module + created hbase file

---
+++ nodejs/routes/hbase.js
@@ -0,0 +1,24 @@
+var hbase = require('hbase');
+
+var db = hbase({ 
+	host: '127.0.0.1', 
+	port: 8080 
+});
+
+module.exports = {
+	getWord: function(word){
+
+	},
+
+	track: function(word){
+
+	},
+
+	untrack: function(word){
+
+	},
+
+	ignore: function(word){
+
+	}
+}
